17f90a24e5d6aefc96e905fc223e457fc7124b61
Leonardo Kirian <leokirian@gmail.com>
2026-06-12 10:35:04 -0300

    adiciona segunda dica

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // CONFIGURAÇÃO DA PALAVRA-CHAVE (Digite em letras minúsculas)
 const KEYWORD_PASSWORD = "cacto"; 
 
+// Variável para contar os erros
+let wrongAttempts = 0;
+
 // Data configurada para 04 de Agosto de 2026 no formato Universal
 const targetDate = new Date("2026-08-04T00:00:00").getTime();
 
@@ -15,6 +18,7 @@ const passwordInput = document.getElementById("password-input");
 const passwordScreen = document.getElementById("password-screen");
 const mainSite = document.getElementById("main-site");
 const passwordError = document.getElementById("password-error");
+const passwordHint = document.getElementById("password-hint"); // Puxando a dica do HTML
 
 function checkPassword() {
     const enteredText = passwordInput.value.toLowerCase().trim();
@@ -27,7 +31,14 @@ function checkPassword() {
         updateTimer();
         setInterval(updateTimer, 1000);
     } else {
-        passwordError.classList.remove("hidden");
+        wrongAttempts++; // Soma 1 aos erros
+        passwordError.classList.remove("hidden"); // Mostra o erro
+        
+        // Se já errou pelo menos 1 vez, mostra a dica
+        if (wrongAttempts >= 1) {
+            passwordHint.classList.remove("hidden");
+        }
+
         passwordInput.value = "";
         passwordInput.focus();
     }
